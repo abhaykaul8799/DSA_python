1842d0c122c43f7206aa6a96eded191ce2cfc501
Abhay Kaul <abhaykaul8799@gmail.com>
2020-03-25 03:41:25 +0530

    modified parentheses check code

--- Stacks/parentheses-balance-check.py
+++ Stacks/parentheses-balance-check.py
@@ -39,7 +39,8 @@ def balance_check2(s):
         else:
             if len(stack) == 0:
                 return False
-            elif (stack.pop(),paren) not in matches:
+            last_open = stack.pop()
+            if (last_open,paren) not in matches:
                 return False
     return len(stack) == 0
 from nose.tools import assert_equal
